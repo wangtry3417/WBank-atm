650d365ae6c05bee2252dc1db849355a65104b54
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-05-18 07:34:09 +0000

    wbank-atm-v1

---
+++ main.py
@@ -0,0 +1,43 @@
+from requests import get, patch
+import json, hashlib
+
+cardNumber = "0107a5de48a35bcc6d03535d00478e29e089a92f1104ac5d40efb5bdfb2435d4"
+
+def write_data():
+  auth_url = "https://wtechhk.com/wbank/v2/csrftoken"
+  token = get(url=auth_url, headers={"Authorization": "Bearer wtech666"}).json()["token"]
+  r = get(url="https://wtechhk.com/wbank/auth/v1/session", data={"user": "benchan609", "pw": "Chan1234#", "url": "/wbank/card/action", "csrf_token": token})
+  if r.status_code != 200: print(r)
+  data = json.dumps(r.json(), indent=4)
+  with open("data.json", "w") as w:
+    w.write(data)
+    w.close()
+
+def wpay_payment():
+  with open("data.json", "r") as fp:
+    data = json.load(fp)
+    data["password"] = data["loginPw"]
+    data["cardNumber"] = hashlib.sha256(f"{data["accnumber"]}->{data["password"]}".encode("utf-8")).hexdigest()
+    data["accessKey"] = "0988"
+    data["reviewer"] = "boc.hk"
+    data["amount"] = "1000"
+    pmReq = patch(url="https://wtechhk.com/wbank/card/action", json=data)
+    if pmReq.status_code != 200: print(pmReq.json())
+    else: print(pmReq.json())
+    fp.close()
+write_data()
+"""
+url = "https://wtechhk.com/wbank/card/action"
+data = {"cardNumber": cardNumber,
+        "password": "Chan1234#",
+        "accessKey": "09988",
+        }
+resp = get(url=url)
+if resp.status_code != 200: print(resp)
+else: 
+  password = resp.json()["loginPw"]
+  paymentData = {"cardNumber": cardNumber, "password": password, "accessKey": "0988", "reviewer": "wbank", "amount": "100"}
+  res = patch(url=url, json=paymentData, headers={"Content-Type": "application/json"})
+  if res.status_code != 200: print(res)
+  else: print(res.json())
+  """
---
+++ server.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, jsonify, request
+import json, requests, hashlib
+from main import write_data
+
+app = Flask(__name__, template_folder="pages")
+
+@app.route("/")
+def index():
+    return render_template("atm.html")
+
+@app.route("/api/card")
+def res_card():
+    write_data()
+    with open("data.json", "r") as fp:
+        return jsonify(json.load(fp))
+
+@app.route("/api/transaction", methods=["GET", "POST"])
+def transaction():
+    if not request.json: return jsonify(msg="沒有東西")
+    type = request.json.get("type")
+    amount = request.json.get("amount")
+    data = None
+    with open("data.json", "r") as fp:
+        data = json.load(fp)
+        fp.close()
+    if type == "withdraw":
+        data["password"] = data["loginPw"]
+        data["cardNumber"] = hashlib.sha256(f"{data["accnumber"]}->{data["password"]}".encode("utf-8")).hexdigest()
+        data["accessKey"] = "12309"
+        data["reviewer"] = "wbank"
+        data["amount"] = str(amount)
+        resp = requests.patch(url="https://wtechhk.com/wbank/card/action", json=data)
+        if resp.status_code != 200: return jsonify(newBalance=data["balance"], message="請求失敗", error=True)
+        return jsonify(newBalance=data["balance"], message=resp.json()["message"])
+    elif type == "deposit":
+        data["password"] = data["loginPw"]
+        data["cardNumber"] = hashlib.sha256(f"{data["accnumber"]}->{data["password"]}".encode("utf-8")).hexdigest()
+        resp = requests.get(url="https://wtechhk.com/wbank/hash/transfer", headers={"user":"wbank", "reviewer":data["loginUser"], "amount": str(amount)})
+        if resp.status_code != 200: return jsonify(msg="Error")
+        res = requests.get(url="https://wtechhk.com/wbank/card/action", headers={"cardNumber": data["cardNumber"], "password": data["password"]})
+        if resp.status_code != 200: return jsonify(msg=str(res.content.decode("utf-8")))
+        return jsonify(balance=res.json()["balance"])
+    return jsonify(msg="請求方式不支援", code=400)
+
+app.run(host="0.0.0.0")
